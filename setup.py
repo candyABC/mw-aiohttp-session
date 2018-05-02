a884fc6e81bcc933280dca190241ffb10b0170d9
candyabc <hfcandyabc@163.com>
2018-05-02 10:12:31 +0800

    add

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 import os
+import codecs
+import re
 
 
 install_requires = ['aiohttp>=3.0.1']
@@ -7,11 +9,18 @@ extras_require = {
     'aioredis': ['aioredis>=1.0.0'],
 }
 
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
+        __file__)), 'aiohttp_session', '__init__.py'), 'r', 'latin1') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'$", fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
 def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
 setup(name='aiohttp-session',
-      version='0.0.1',
+      version=version,
       description=("only for maxwin aiohttp session"),
       long_description='\n\n'.join((read('README.md'))),
       classifiers=[
